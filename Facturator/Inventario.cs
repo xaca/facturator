c215ae7c4f59b22f98aab7bdfb5c7011087a46eb
Xaca <and1res@hotmail.com>
2019-05-26 07:51:07 -0500

    Se agrega la clase inventario, para controlar el stock

---
+++ Facturator/Inventario.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facturator {
+    class Inventario {
+        private Producto[] productos;
+        private int indice;
+
+        public Inventario() 
+        {
+            productos = new Producto[100];//TODO: Convertir en una lista
+
+        }
+
+        public void AgregarProducto(Producto producto)
+        {
+
+            if (indice + 1 <= productos.Length)
+            {
+                productos[indice++] = producto;
+            }
+        }
+
+        public void AgregarProducto(string nombre, float precio, int cantidad)
+        {
+            AgregarProducto(new Producto(nombre, precio, cantidad));
+        }
+
+    }
+}
